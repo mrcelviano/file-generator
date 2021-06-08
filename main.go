96fb96434435eda7f05094ca96ddc23d6d0b6b5f
mrcelviano <nikita.sergienko@enkod.io>
2021-06-08 12:24:42 +0300

    update_logic_generator

--- main.go
+++ main.go
@@ -2,30 +2,90 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"gopkg.in/urfave/cli.v1"
 	"os"
-	"time"
-)
-
-var (
-	fileMask    = "test_file_"
-	numberFiles = 10
-	fileSize    = 25e4
-	filePath    = "files"
 )
 
 func main() {
-	start := time.Now()
+	app := cli.NewApp()
+	app.Name = "file-generator"
+	app.Usage = "File Generation Application"
+	app.Commands = []cli.Command{
+		{
+			Name: "generate", ShortName: "g",
+			Usage: "Generate files",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "fileName, fn",
+					Usage: "Specifies the name of the files. The default name is 'test'",
+					Value: "test",
+				},
+				cli.StringFlag{
+					Name:  "filePath, fp",
+					Usage: "Specifies the path to files. The default path is 'files'",
+					Value: "files",
+				},
+				cli.Int64Flag{
+					Name:  "fileSize, fs",
+					Usage: "Specifies the size of files. Default size 10kb",
+					Value: 1e4,
+				},
+				cli.Int64Flag{
+					Name:  "numberFiles , nf",
+					Usage: "Sets the number of files to generate. Default amount 10",
+					Value: 10,
+				},
+			},
+			Action: func(c *cli.Context) error {
+				var (
+					fileName    = c.String("fileName")
+					filePath    = c.String("filePath")
+					fileSize    = c.Int("fileSize")
+					numberFiles = c.Int("numberFiles")
+				)
 
-	for i := 1; i < numberFiles+1; i++ {
-		file, err := os.Create(fmt.Sprintf("%v/%v%v", filePath, fileMask, i))
-		if err != nil {
-			log.Panic(err)
-		}
-		defer file.Close()
-		data := make([]byte, int(fileSize), int(fileSize))
-		file.Write(data)
+				err := os.Mkdir(filePath, os.ModeDir)
+				if err != nil {
+					return err
+				}
+
+				for i := 1; i < numberFiles+1; i++ {
+					file, err := os.Create(fmt.Sprintf("%v/%v%v", filePath, fileName, i))
+					if err != nil {
+						return err
+					}
+					defer file.Close()
+					data := make([]byte, fileSize, fileSize)
+					file.Write(data)
+				}
+
+				return nil
+			},
+		},
+		{
+			Name: "delete", ShortName: "d",
+			Usage: "Delete files",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "deleteDirectory, dir",
+					Usage: "Delete all files in a directory. The default directory is 'files'",
+					Value: "files",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				var (
+					fileDirectory = c.String("deleteDirectory")
+				)
+
+				err := os.RemoveAll(fileDirectory)
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
+		},
 	}
 
-	log.Println(time.Since(start))
+	app.Run(os.Args)
 }
